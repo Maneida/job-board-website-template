03bd94e5d0ee0f6555d2d1bf7544079b0555c2fb
Michael Amuah <mkdamuah@hotmail.com>
2024-03-22 17:21:55 +0000

    added bootstrap, started with dynamic data

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Accra, Ghana',
+        'salary': 'GHS 4000'
+    },
+    {
+        'id': 2,
+        'title': 'Software Developer',
+        'location': 'Kumasi, Ghana',
+        'salary': 'GHS 6500'
+    },
+    {
+        'id': 3,
+        'title': 'Softwae Engineer',
+        'location': 'Remote',
+        'salary': 'GHS 8000'
+    },
+    {
+        'id': 4,
+        'title': 'Data Entry Clerk',
+        'location': 'Sunyani, Ghana',
+        'salary': 'GHS 1500'
+    }
+]
+
 @app.route('/', strict_slashes=False)
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
