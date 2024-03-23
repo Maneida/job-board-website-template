d5d08a0fdefadcec3ce2a31360009f73bd97490a
Michael Amuah <mkdamuah@hotmail.com>
2024-03-23 11:47:42 +0000

    dynamic content

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -26,12 +26,35 @@ JOBS = [
         'title': 'Data Entry Clerk',
         'location': 'Sunyani, Ghana',
         'salary': 'GHS 1500'
+    },
+    {
+        'id': 5,
+        'title': "Miriam Tagoe's TA",
+        'location': 'Miotso, Ghana',
+        'salary': 'GHS 1800'
+    }
+    ,
+    {
+        'id': 6,
+        'title': "Ekua Thompson's Lab Assistant",
+        'location': 'Ho, Ghana',
+        'salary': 'GHS 1800'
+    },
+    {
+        'id': 7,
+        'title': "Assistant to the Executive Assistant",
+        'location': 'Appolonia City, Ghana',
+        'salary': 'GHS 2400'
     }
 ]
 
 @app.route('/', strict_slashes=False)
 def hello_world():
     return render_template("home.html", jobs=JOBS)
+
+@app.route('/jobs', strict_slashes=False)
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
