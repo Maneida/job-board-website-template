8737f7547589eb00d362e5ff0e9a787d498520bc
Michael Amuah <mkdamuah@hotmail.com>
2024-03-31 00:16:12 +0000

    added basic jobpage with other structure improvements

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 from datetime import datetime
 from flask import Flask, render_template, jsonify
-import database
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    jobs = database.load_jobs_from_db()
+    jobs = load_jobs_from_db()
     return render_template("home.html", jobs=jobs, company_name='Jovian',
                            year=datetime.now().year)
 
 
 @app.route('/api/v1/jobs', strict_slashes=False)
 def list_jobs():
-    jobs = database.load_jobs_from_db()
+    jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route('/job/<id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not found", 404
+    return render_template("jobpage.html", job=job, company_name='Jovian')
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -25,3 +25,17 @@ def load_jobs_from_db():
             job_dict = dict(zip(keys, row_values))
             jobs.append(job_dict)
         return jobs
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(f"SELECT * FROM jobs WHERE id = {id}"))
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            keys = ['id', 'title', 'location', 'salary',
+                    'currency', 'responsibilities', 'requirements', 'application_deadline']
+            job = dict(zip(keys, rows[0]))
+            return job
