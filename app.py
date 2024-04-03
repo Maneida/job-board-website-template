44ff726788b5a41f6552a28363e5d3ee0303d8f6
Michael Amuah <mkdamuah@hotmail.com>
2024-04-03 00:26:11 +0000

    connected application to database with other improvements

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 from datetime import datetime
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
+
+year = datetime.now().year
 
 
 @app.route('/', strict_slashes=False)
 def hello_world():
     jobs = load_jobs_from_db()
     return render_template("home.html", jobs=jobs, company_name='Jovian',
-                           year=datetime.now().year)
+                           year=year)
 
 
 @app.route('/api/v1/jobs', strict_slashes=False)
@@ -18,12 +20,24 @@ def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
 
-@app.route('/job/<id>')
+
+@app.route('/job/<id>', strict_slashes=False, methods=['POST', 'GET'])
 def show_job(id):
+    if request.method == 'POST':
+        data = request.form
+        data = dict(data)
+        if add_application_to_db(id, data):
+            return render_template("application_submitted.html",
+                                   company_name='Jovian', application=data,
+                                   year=year)
+        else:
+            return "Failed to submit application"
+
     job = load_job_from_db(id)
     if not job:
         return "Not found", 404
-    return render_template("jobpage.html", job=job, company_name='Jovian')
+    return render_template("jobpage.html", job=job, company_name='Jovian',
+                           year=year)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine, text
 import os
 from dotenv import load_dotenv
+from sqlalchemy.exc import SQLAlchemyError
 
 load_dotenv()
 
@@ -39,3 +40,26 @@ def load_job_from_db(id):
                     'currency', 'responsibilities', 'requirements', 'application_deadline']
             job = dict(zip(keys, rows[0]))
             return job
+
+
+def add_application_to_db(job_id, data):
+    try:
+        with engine.connect() as conn:
+            conn.execute(
+                text("INSERT INTO applications (job_id, full_name, email,\
+                    linkedin_url, education, work_experience, resume_url)\
+                        VALUES (:job_id, :full_name, :email, :linkedin_url,\
+                            :education, :work_experience, :resume_url)"),
+                [{"job_id": job_id,
+                  "full_name": data['full_name'],
+                  "email": data['email'],
+                  "linkedin_url": data['linkedin_url'],
+                  "education": data['education'],
+                  "work_experience": data['work_experience'],
+                  "resume_url": data['resume_url']}]
+            )
+            conn.commit()
+        return True
+    except SQLAlchemyError as e:
+        print(f"Error inserting data: {e}")
+        return False
